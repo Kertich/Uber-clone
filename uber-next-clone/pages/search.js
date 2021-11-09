971d4e7c4528158fddb292b6020a1bc469f1616c
Kertich <kipsangkertich@gmail.com>
2021-11-09 14:08:16 +0300

    add search page

---
+++ uber-next-clone/pages/search.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import tw from "tailwind-styled-components";
+
+const Search = () => {
+    return (
+        <Wrapper>
+            <ButtonContainer>
+                <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png"/>
+            </ButtonContainer>
+
+
+            {/* button container */}
+            {/* input container
+            saved places
+            confirm location */}
+        </Wrapper>
+    )
+}
+
+export default Search
+
+const Wrapper = tw.div`
+
+`
